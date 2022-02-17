96ed6cdcc4b7dbd8b076aeec6f796f5a909f27cb
jakegoodman23 <jakegoodman2412@gmail.com>
2022-02-16 21:59:46 -0600

    db test

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ email_password = os.environ.get("EMAIL_PASSWORD")
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///blog.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = 'filesystem'
